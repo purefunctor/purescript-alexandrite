30033004909abf76381ab4d023144f76c7e0e16f
Justin Garcia <git@purefunctor.me>
2026-06-04 00:36:18 +0800

    Add failing test case for type document highlights

---
+++ tests-integration/fixtures/lsp/1780504560_document_highlight_types_and_imports/Lib.purs
@@ -0,0 +1,17 @@
+module Lib where
+
+data Box = Box
+
+class ImportedClass a where
+  importedMember :: a -> Box
+
+foreign import data Product :: Type -> Type -> Type
+
+infixr 6 type Product as :*:
+
+value :: Box
+value = Box
+
+append left right = left
+
+infixr 5 append as <++>
