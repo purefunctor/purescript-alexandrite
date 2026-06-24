b9481ff809fd425452c234450970582970a9d2d0
Justin Garcia <git@purefunctor.me>
2026-06-24 17:21:24 +0800

    Implement new command line parsing

--- compiler-bin/src/cli.rs
+++ compiler-bin/src/cli.rs
@@ -1,4 +1,6 @@
-use clap::{ArgAction, Parser};
+use std::path::PathBuf;
+
+use clap::{ArgAction, Args, Parser, Subcommand};
 use tracing::level_filters::LevelFilter;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
@@ -8,58 +10,102 @@ const VERSION: &str = env!("CARGO_PKG_VERSION");
 pub struct Config {
     #[arg(long)]
     pub stdio: bool,
-    #[arg(long, help("Print log path"))]
+    /// Print log path.
+    #[arg(long)]
     pub log_file: bool,
-    #[arg(
-        long,
-        value_name("LevelFilter"),
-        help("Log level for the query engine"),
-        default_value("off")
-    )]
+    /// Log level for the query engine.
+    #[arg(long, value_name("LevelFilter"), default_value("off"))]
     pub query_log: LevelFilter,
-    #[arg(
-        long,
-        value_name("LevelFilter"),
-        help("Log level for the language server"),
-        default_value("info")
-    )]
+    /// Log level for the language server.
+    #[arg(long, value_name("LevelFilter"), default_value("info"))]
     pub lsp_log: LevelFilter,
-    #[arg(
-        long,
-        value_name("LevelFilter"),
-        help("Log level for the type checker"),
-        default_value("off")
-    )]
+    /// Log level for the type checker.
+    #[arg(long, value_name("LevelFilter"), default_value("off"))]
     pub checking_log: LevelFilter,
-    #[arg(
-        long,
-        help("Command to use to get source files"),
-        long_help("This argument also disables the spago.lock integration")
-    )]
+    /// Command to use to get source files.
+    ///
+    /// This argument also disables the spago.lock integration.
+    #[arg(long)]
     pub source_command: Option<String>,
 
-    #[arg(
-        long,
-        help("Publish diagnostics on textDocument/didOpen"),
-        value_name("bool"),
-        action = ArgAction::Set,
-        default_value_t = true
-    )]
+    /// Publish diagnostics on textDocument/didOpen.
+    #[arg(long, value_name("bool"), action = ArgAction::Set, default_value_t = true)]
     pub diagnostics_on_open: bool,
 
-    #[arg(
-        long,
-        help("Publish diagnostics on textDocument/didSave"),
-        value_name("bool"),
-        action = ArgAction::Set,
-        default_value_t = true
-    )]
+    /// Publish diagnostics on textDocument/didSave.
+    #[arg(long, value_name("bool"), action = ArgAction::Set, default_value_t = true)]
     pub diagnostics_on_save: bool,
 
-    #[arg(
-        long,
-        help("Publish diagnostics on textDocument/didChange (opt-in)"),
-        default_value_t = false
-    )]
+    /// Publish diagnostics on textDocument/didChange (opt-in).
+    #[arg(long, default_value_t = false)]
     pub diagnostics_on_change: bool,
+}
+
+#[derive(Debug, Parser)]
+#[command(about, version(VERSION))]
+pub struct Cli {
+    /// Print log path.
+    #[arg(long)]
+    pub log_file: bool,
+    #[command(subcommand)]
+    pub command: Option<Command>,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum Command {
+    /// Run the language server.
+    Lsp(LspOptions),
+    /// Generate documentation.
+    Docs(DocsOptions),
+}
+
+#[derive(Debug, Args)]
+pub struct LoggingOptions {
+    /// Log level for the query engine.
+    #[arg(long, value_name("LevelFilter"), default_value("off"))]
+    pub query_log: LevelFilter,
+
+    /// Log level for the type checker.
+    #[arg(long, value_name("LevelFilter"), default_value("off"))]
+    pub checking_log: LevelFilter,
+}
+
+#[derive(Debug, Args)]
+pub struct LspOptions {
+    #[command(flatten)]
+    pub logging: LoggingOptions,
+
+    #[arg(long)]
+    pub stdio: bool,
+
+    /// Log level for the language server.
+    #[arg(long, value_name("LevelFilter"), default_value("info"))]
+    pub lsp_log: LevelFilter,
+
+    /// Command to use to get source files.
+    ///
+    /// This argument also disables the spago.lock integration.
+    #[arg(long)]
+    pub source_command: Option<String>,
+
+    /// Publish diagnostics on textDocument/didOpen.
+    #[arg(long, value_name("bool"), action = ArgAction::Set, default_value_t = true)]
+    pub diagnostics_on_open: bool,
+
+    /// Publish diagnostics on textDocument/didSave.
+    #[arg(long, value_name("bool"), action = ArgAction::Set, default_value_t = true)]
+    pub diagnostics_on_save: bool,
+
+    /// Publish diagnostics on textDocument/didChange.
+    #[arg(long, default_value_t = false)]
+    pub diagnostics_on_change: bool,
+}
+
+#[derive(Debug, Args)]
+pub struct DocsOptions {
+    #[command(flatten)]
+    pub logging: LoggingOptions,
+    /// Output directory for the generated documentation.
+    #[arg(long, value_name("DIR"), default_value("docs"))]
+    pub output_folder: PathBuf,
 }
