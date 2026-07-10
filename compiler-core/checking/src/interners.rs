7c627119320a767e46cefd8a10e64ab5e35db60c
Amp <amp@ampcode.com>
2026-07-10 22:38:07 +0000

    Inline checking interner operations
    Co-authored-by: Justin Garcia <hello@purefunctor.me>

--- compiler-core/checking/src/interners.rs
+++ compiler-core/checking/src/interners.rs
@@ -11,34 +11,42 @@ pub struct CoreInterners {
 }
 
 impl CoreInterners {
+    #[inline]
     pub fn intern_type(&self, t: Type) -> TypeId {
         self.types.intern(t)
     }
 
+    #[inline]
     pub fn lookup_type(&self, id: TypeId) -> Type {
         self.types[id].clone()
     }
 
+    #[inline]
     pub fn intern_forall_binder(&self, b: ForallBinder) -> ForallBinderId {
         self.forall_binders.intern(b)
     }
 
+    #[inline]
     pub fn lookup_forall_binder(&self, id: ForallBinderId) -> ForallBinder {
         self.forall_binders[id]
     }
 
+    #[inline]
     pub fn intern_row_type(&self, r: RowType) -> RowTypeId {
         self.row_types.intern(r)
     }
 
+    #[inline]
     pub fn lookup_row_type(&self, id: RowTypeId) -> RowType {
         self.row_types[id].clone()
     }
 
+    #[inline]
     pub fn intern_smol_str(&self, s: SmolStr) -> crate::core::SmolStrId {
         self.smol_strs.intern(s)
     }
 
+    #[inline]
     pub fn lookup_smol_str(&self, id: crate::core::SmolStrId) -> SmolStr {
         self.smol_strs[id].clone()
     }
