1fd99f917a325792f2c7efecf2769be5bfad6a85
Justin Garcia <hello@purefunctor.me>
2026-08-06 15:11:50 +0000

    Add failing test case for instance head completions

---
+++ tests-integration/fixtures/lsp/1786028520_completion_instance_head/Main.purs
@@ -0,0 +1,23 @@
+module Main where
+
+data FunctorInput a
+
+class FoldableIdentity a
+
+type Cached = F
+--            ^
+
+instance 
+--       ^
+
+instance F
+--        ~
+
+instance functorIdentity :: 
+--                          ^
+
+instance functorIdentity2 :: F
+--                            ^
+
+instance Support.F
+--                 ^
